673657860e8bc4e9cba98f8588a475e3cd2ae753
Evan Waldmann <waldmannevan@knights.ucf.edu>
2018-10-05 13:09:02 -0400

    sort of finished

--- RtutorialCMS.R
+++ RtutorialCMS.R
@@ -8,9 +8,16 @@ x <- 1 + 1 # assignment
 x 
 x %% 2  # mod
 
+y <- 1:10
+y^2 
+sum(y)
+
 getwd()
 setwd('../Desktop/RtutorialCMS')
 
+#There is a ton of information online about how to use R, here are a few links:
+#https://rcompanion.org/rcompanion/a_06.html 
+#https://www.tutorialspoint.com/r/r_data_types.htm
 
 # Matrices and Vectors in R ---- 
 x <- c(1,2,3,4,5,6,7,8,9)
@@ -38,6 +45,8 @@ squarePlusOne <- function(x){
   return(x^2 +1)
 }
 squarePlusOne(4)
+
+plot( -50:50,squarePlusOne(-50:50))
 
 #using a function from another R file  (that you made)
 source('externalFunction.R')  # you can include the path to the file if you need to
@@ -77,7 +86,16 @@ plot(out, xlab = "time", ylab = "-")
 plot(out[, "X"], out[, "Z"], pch = ".")
 mtext(outer = TRUE, side = 3, "Lorenz model", cex = 1.5)
 
+#derivatives
+install.packages("Deriv")
+library(Deriv)
 
+g <- function(x) x^5 
+gdx <- Deriv(g)
+gdx(2)
+
+f <- function(x, y) sin(x) * cos(y)
+Deriv(f)
 
 
 ## Reading/writing data sets and some graphing ---- 
@@ -85,7 +103,13 @@ mtext(outer = TRUE, side = 3, "Lorenz model", cex = 1.5)
 pkdata <- read.csv("pokemon.csv", header =T)
 
 head(pkdata) # look at the data 
+nrow(pkdata)
+ncol(pkdata)
 
+summary(pkdata)
+
+pkdata[2:19]<- NULL
+pkdata["japanese_name"]<- NULL
 
 plot(pkdata$base_happiness, pkdata$weight_kg )
 plot(pkdata$percentage_male,pkdata$attack)
@@ -94,16 +118,34 @@ plot(pkdata$percentage_male,pkdata$weight_kg)
 pkdata.sorted <- pkdata[order(pkdata$pokedex_number),]
 
 
-unique( sapply(unlist(pkdata$abilities), paste) )
+#string manipulations 
+install.packages("gdata")
+library(gdata)
+text <- gsub("\\[|\\]|'|'", "", pkdata$abilities)  # delete weird character formatting 
+fixedtext<- trim(unlist(lapply(text, strsplit, split=","))) # combine strings into one array and "trim" spaces from front and back
+
+summary(as.data.frame(fixedtext))
+
+unique(fixedtext)
+
 
 ## statistics in R ---- 
 
 
+df = data.frame(happy = pkdata$base_happiness , weight = pkdata$weight_kg )
 
+cor.test( ~ happy + weight, 
+          data=df,
+          method = "pearson",
+          continuity = TRUE,
+          conf.level = 0.95)
 
+## Better graphing in R ---- 
+install.packages("ggplot2")
+library(ggplot2)
 
+ggplot(, aes(-50:50, squarePlusOne(-50:50))) + geom_line(size =2) + theme_bw()+ labs(y="x^2 +1", x="x", title= "Graph")
 
-
-
+#ggplot(pkdata, aes(pkdata$base_happiness, pkdata$weight_kg, color=(pkdata$base_happiness))) + geom_point() + geom_boxplot(aes(unique(pkdata$base_happiness)))
 
 
